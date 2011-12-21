7d958c214e446062fd4409487d008ddca296ac0e
Johannes Hofmann-Wellenhof <gianni.hw@gmail.com>
2011-12-21 09:16:27 +0100

    added comments

--- src/EmptyStorageException.java
+++ src/EmptyStorageException.java
@@ -1,28 +1,54 @@
-
+/**
+ * Class EmptyStorage Exception extends Exception
+ * 
+ * @author Gruppe222
+ * 
+ */
 public class EmptyStorageException extends Exception {
 
 	/**
-	 * 
+	 * Automatically generated ID.
 	 */
 	private static final long serialVersionUID = -2614163354604579142L;
 
+	/**
+	 * Constructor without parameter calls the super constructor
+	 */
 	public EmptyStorageException() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
+	/**
+	 * Constructor with String as parameter. Calls the super constructor.
+	 * 
+	 * @param arg0
+	 *            a String
+	 */
 	public EmptyStorageException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Constructor with Throwable as parameter. Calls the super constructor.
+	 * 
+	 * @param arg0
+	 *            a Throwable
+	 */
 	public EmptyStorageException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Constructor with String and Throwable as parameters. Calls the super
+	 * constructor.
+	 * 
+	 * @param arg0
+	 *            a String
+	 * @param arg1
+	 *            a Throwable
+	 */
 	public EmptyStorageException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 }
